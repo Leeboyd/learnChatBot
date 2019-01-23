d52b17af5360f7425b6a6ac10ba61025b380c233
LeeBoy <jobboy19890101@gmail.com>
2019-01-24 07:03:21 +0800

    remove Hard-coding the Slack token

--- src/logbot.js
+++ src/logbot.js
@@ -1,15 +1,31 @@
 var Botkit = require('botkit')
+var fs = require('fs')
 
 var controller = Botkit.slackbot({debug: false})
-controller
-  .spawn({
-    token: '' // token!
-  })
-  .startRTM(function (err) {
-    if (err) {
-      throw new Error(err)
-    }
-  })
+
+// START: Load Slack token from file.
+if (!process.env.slack_token_path) {
+  console.log('Error: Specify slack_token_path in env')
+  process.exit(1)
+}
+
+fs.readFile(process.env.slack_token_path, function (err, data) {
+  if (err) {
+    console.log('Error: Specify token in slack_token_path file');
+    process.exit(1)
+  }
+  data = String(data)
+  data = data.replace(/\s/g, '')
+  controller
+    .spawn({token: data})
+    .startRTM(function (err) {
+      if (err) {
+        throw new Error(err)
+      }
+    })
+})
+
+// END: Load Slack token from file.
 
 controller.hears(['hello', 'hi'], ['direct_message', 'direct_mention', 'mention'], function (bot, message) {
   bot.reply(message, 'BBBB. :robot_face:')
